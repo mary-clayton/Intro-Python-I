a1186a36870cfd5760b691a18bfc14eb09a36b32
mary-clayton <mary.cathlean@gmail.com>
2020-09-02 19:20:16 -0700

    5-7

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,30 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
-print(x)
+x.append(4)
+# print(x)
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
-print(x)
+for a in y:
+    x.append(a)
+# print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
-print(x)
+x.remove(8)
+# print(x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
-print(x)
+x.insert(5, 99)
+# print(x)
 
 # Print the length of list x
 # YOUR CODE HERE
+print(len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+for a in x:
+    print(x * 1000)
